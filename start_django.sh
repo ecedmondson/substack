392fb1d531e5b5e010c763bebb928c93efebd693
Emily Edmondson <ecedmondson@gmail.com>
2025-01-26 18:05:02 -0600

    Docker

---
+++ start_django.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#MAX_RETRIES=5
+#RETRIES=0
+#until poetry run python manage.py migrate; do
+#    RETRIES=`expr $RETRIES + 1`
+#    if [[ "$RETRIES" -eq "$MAX_RETRIES" ]]; then
+#        echo "Retry Limit Exceeded. Aborting..."
+#        exit 1
+#    fi
+#    sleep 2
+#done
+
+poetry run python manage.py collectstatic
+poetry run python manage.py runserver 0.0.0.0:8000
