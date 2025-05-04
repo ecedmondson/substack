263441730a78f6618d5e854a3bbae9192f744bd5
Emily Edmondson <ecedmondson@gmail.com>
2025-05-03 20:58:09 -0500

    settings just a copy, less actually

---
+++ sms_relay/settings/connection_settings.py
@@ -0,0 +1,14 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings
+
+
+class DBConnectionSettings(BaseSettings):
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: SecretStr = SecretStr("password")
+    POSTGRES_HOST: str = "localhost"
+    POSTGRES_NAME: str = "postgres"
+    POSTGRES_PORT: int = 5432
+
+    @property
+    def url(self) -> str:
+        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD.get_secret_value()}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_NAME}"
