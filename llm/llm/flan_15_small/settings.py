d7ee3d87bbcc829ba31b8850bb5b70d569fd9263
Emily Edmondson <ecedmondson@gmail.com>
2025-05-21 20:52:36 -0500

    flan 15 small

--- llm/llm/flan_15_small/settings.py
+++ llm/llm/flan_15_small/settings.py
@@ -1,6 +1,7 @@
 from settings.model_settings import ModelSettingsBase
-from llm.interface import ModelInterface
+from settings.interface_types import ModelInterfaceType
+from llm.interface import model_interface_factory
 
-config = ModelSettingsBase(MODEL_NAME="flan-t5-small", ORGANIZATION_NAME="google")
+config = ModelSettingsBase(MODEL_NAME="flan-t5-small", ORGANIZATION_NAME="google", MODEL_TYPE=ModelInterfaceType.SEQ2SEQ)
 
-flan_interface = ModelInterface(config)
+flan_interface = model_interface_factory(config.MODEL_TYPE)(config)
