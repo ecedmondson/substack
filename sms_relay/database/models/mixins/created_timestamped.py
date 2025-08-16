c50b7689a527e52e5d0704115ca059b88022116c
Emily Edmondson <ecedmondson@gmail.com>
2025-08-15 21:59:17 -0500

    remove leftover schema

--- sms_relay/database/models/mixins/created_timestamped.py
+++ sms_relay/database/models/mixins/created_timestamped.py
@@ -3,12 +3,7 @@ from datetime import datetime
 from pydantic import BaseModel, Field
 
 from sqlalchemy.orm import Mapped, mapped_column
-from database.models.mixins.pydantic_base import PydanticBase
 
 
 class CreatedTimestamped:
     created: Mapped[datetime] = mapped_column(default=datetime.utcnow, index=True)
-
-
-class CreatedTimestampedPydanticMixin(PydanticBase):
-    created: datetime = Field(..., read_only=True)
