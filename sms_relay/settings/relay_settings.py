408834fa0e32f19dccd9d22c786f0bea9841f93b
Emily Edmondson <ecedmondson@gmail.com>
2025-08-23 17:52:42 -0500

    update telnyx settings

--- sms_relay/settings/relay_settings.py
+++ sms_relay/settings/relay_settings.py
@@ -4,4 +4,6 @@ from pydantic_settings import BaseSettings
 
 class TelnyxSettings(BaseSettings):
     TELNYX_API_KEY: SecretStr = SecretStr("SUPER-SECRET-KEY")
+    TELNYX_DEFAULT_NUMBER: str = "867-5309"
+    TELNYX_DEFAULT_PROFILE: str = "SMS_RELAY"
 
